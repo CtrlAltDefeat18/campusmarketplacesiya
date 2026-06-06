f2c48c59cdbfc579aeefe3478bfee4a19b23f61e
Aphelele Mjobo <215697121+CtrlAltDefeat18@users.noreply.github.com>
2026-06-07 00:02:14 +0200

    Update Cloudinary configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Your real config — fill in the two values below.
    (This file is git-ignored so it won't be committed.) */
 window.CM_CONFIG = {
-  cloudinaryCloud: "PASTE_YOUR_CLOUD_NAME_HERE",
-  cloudinaryPreset: "PASTE_YOUR_UNSIGNED_PRESET_HERE",
+  cloudinaryCloud: "dtzubrdee",
+  cloudinaryPreset: "marketplace listings",
 };
